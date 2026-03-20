e1ad896e86d9b7eb2c893f169da54b036d734bf4
BorisDahms <boris.dahms@gmail.com>
2026-03-20 20:12:55 +0100

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function HomePage() {
-  return (
-    <iframe
-      src="/generator.html"
-      title="Bewerbungsgenerator"
-      style={{ width: "100%", height: "100vh", border: "none" }}
-    />
-  );
-}
