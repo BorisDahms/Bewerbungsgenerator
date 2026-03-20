3b5131dc1b21c4644ff6215812693a1f0e0c4343
BorisDahms <boris.dahms@gmail.com>
2026-03-20 19:41:05 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
+import { verifyToken } from "./lib/verifyToken";
 
 export function middleware(req: NextRequest) {
-  return NextResponse.next();
+  const tokenFromUrl = req.nextUrl.searchParams.get("token");
+  const tokenFromCookie = req.cookies.get("access_token")?.value ?? null;
+
+  if (verifyToken(tokenFromUrl)) {
+    const cleanUrl = req.nextUrl.clone();
+    cleanUrl.searchParams.delete("token");
+
+    const response = NextResponse.redirect(cleanUrl);
+    response.cookies.set("access_token", tokenFromUrl as string, {
+      httpOnly: true,
+      sameSite: "lax",
+      secure: true,
+      path: "/",
+    });
+
+    return response;
+  }
+
+  if (verifyToken(tokenFromCookie)) {
+    return NextResponse.next();
+  }
+
+  return new NextResponse("Nicht autorisiert", { status: 401 });
 }
 
 export const config = {
-  matcher: ["/"],
+  matcher: ["/", "/generator.html"],
 };
