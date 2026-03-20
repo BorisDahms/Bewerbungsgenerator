f9eec54f5dd0fa685badbb4a7a09d79354506ebd
BorisDahms <boris.dahms@gmail.com>
2026-03-20 19:28:44 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
 export default function HomePage() {
-  return <div>Test</div>;
+  return (
+    <iframe
+      src="/generator.html"
+      title="Bewerbungsgenerator"
+      style={{ width: "100%", height: "100vh", border: "none" }}
+    />
+  );
 }
